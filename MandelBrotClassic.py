e57209eace48b24bc72694577a12cead3683c5bd
Alessandra Larrubia <62728284+larrubia@users.noreply.github.com>
2023-01-08 18:40:05 +0000

    Add files via upload

---
+++ MandelBrotClassic.py
@@ -0,0 +1,65 @@
+'''
+Classic Mandelbrot Set Code 
+DEVICE INFO: Macbook PRO 16GB 2,6 GHz 6-Core Intel Core i7
+macOS: Ventura
+@author Alessandra Larrubia
+'''
+
+import numpy as np
+from timeit import default_timer as timer
+from PIL import Image
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Fractals are infinitely repeating patterns on different scales
+
+# The function calculates whether or not the point of x and Y passed as parameters belongs to the Mandelbrot set
+def mandelbrotset(xvalue, yvalue, maxiter): 
+    nReal = xvalue # saves the xvalue parameter passed 
+    nImag = yvalue # saves the yvalue parameter passed 
+    for n in range(maxiter):
+        nReal2 = nReal * nReal
+        Nimag2 = nImag * nImag
+        nImag = 2 * nReal * nImag + yvalue
+        nReal = nReal2 - Nimag2 + xvalue
+        if nReal2 + Nimag2 > 4.0:
+            break
+    return n # returns the number of iterations necessary to determine the pertinence of the values for Mandelbrot set
+
+if __name__ == '__main__': # allows to execute code when the file runs as a script, but not when it’s imported as a module
+    # defines the size of the axes X and Y: real and imaginary axes (complex numbers)
+    xSize = ySize = 2048 #defines a finite number of pixels on axes x and y.
+    x0 = -2.00 # defines the begin of the real axis X: from x0
+    y0 = -1.25 # defines the begin of the imaginary axis Y: from y0
+    x1 = 0.50 # defines the end of the real axis X: to x1
+    y1 = 1.25 # defines the end of the imaginary axis Y: to y1
+
+    maxIterations = 256 # defines the maximun number of interactions
+
+    pixelWidth = (x1-x0) / xSize # defines each pixel width
+    pixelHeight = (y1-y0) / ySize # defines each pixel height
+
+    print ()
+    print ('---------------------------------------------------------------------------------------------------------------------------------------------------------')
+    print ('ALGORITHM INFO')
+    print ()
+    print('Generating fractal image of size {0} x {1} with {2} interactions'.format(xSize, ySize, maxIterations))
+    
+    start = timer() # saves the processing start time
+    
+    img = np.zeros((xSize, ySize), dtype = np.int32) # defines the domain plan
+    for j in range(ySize): # for every col: associates values to theb axis Y (cy) 
+        cy = y0 + j*pixelHeight
+        for i in range(xSize): # for every row: associates values to the axis X (cx)
+            cx = x0 + i * pixelWidth
+            mandelbrotExec = mandelbrotset(cx, cy, maxIterations) # assign the function mandelbrotset, pass the attributes and execute it
+            img[j, i] = mandelbrotExec # creates the domain plan through the result returned by the function mandelbrotset and keeped in mandelbrotExec
+
+    duration = timer() - start # calculates the whole processing time
+    print(f'Execution time: {duration:.2f} seconds')
+    print ()
+    print ('---------------------------------------------------------------------------------------------------------------------------------------------------------')
+    plt.axis("off") # disables the display of the number line of the axes
+    plt.imshow(img, plt.cm.get_cmap('twilight_shifted')) # plots the mandelbrot figure through the colorset indicated
+    plt.show() # displays the mandelbrot figure
+
---
+++ MandelbrotOpenCL.py
@@ -0,0 +1,106 @@
+'''
+Mandelbrot set code using GPU processing with PyOpenCL
+DEVICE INFO: Macbook PRO 16GB 2,6 GHz 6-Core Intel Core i7
+GRAPHICS: AMD Radeon Pro 5300M 4 GB
+macOS: Ventura
+@author Alessandra Larrubia
+'''
+
+import pyopencl as cl
+import numpy as np
+from PIL import Image
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from timeit import default_timer as timer
+
+width = 2048    #define a finite number of pixels on axis X
+height = 500   #define a finite number of pixels on axis Y
+maxIt = 256    #define the maximun number of interactions allowed
+
+
+#The kernel calculates in parallel if each value point on axis X if belongs to the complex plane.
+#If it belongs to the complex plane, 0 is stored in buffer out (i.e the value belongs to the Mandelbrot set).
+
+KernelSource = '''
+__kernel void mandelbrot(const int width,
+                         const int height,
+                         const int maxIt,
+                         __global int *out
+                        )
+{
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    if (x >= width || y >= height) {
+        return;
+    }
+    double nReal1 = x * 3.0 / (width - 1) - 2.0;  //for every row: associates values to the axis x
+    double nImag1 = y * 3.0 / (height - 1) - 1.5; //for every col: associates values to the axis Y
+    double nReal2 = 0.0;
+    double nImag2 = 0.0;
+    double tmp_x_real;
+    double norm;
+    int divergence_at = 0;
+
+    for (int i = 1; i <= maxIt; ++i) {
+        tmp_x_real = nReal2 * nReal2 - nImag2 * nImag2 + nReal1;
+        nImag2 = 2 * nReal2 * nImag2 + nImag1;
+        nReal2 = tmp_x_real;
+        // if norm > 4.0, we can be sure that the sequence diverges
+        norm = nReal2 * nReal2 + nImag2 * nImag2;
+        if (norm > 4.0) {
+            divergence_at = i;
+            break;
+        }
+    }
+    out[y * width + x] = divergence_at;
+}
+'''
+
+
+def main():
+    
+    #Define open-cl queue and context
+    platform = cl.get_platforms()[0] #Select the platform: Apple at 0x7fff0000
+    device = platform.get_devices()[2] #Select the device on the previous platform: AMD Radeon Pro 5300M Compute Engine on 'Apple' at 0x1021e00>
+    context =cl.Context([device]) #Create the context with the devices informed previously
+    queue = cl.CommandQueue(context) #Create the queue to be used for processing using the selected device set up
+    
+    print ()
+    print ('---------------------------------------------------------------------------------------------------------------------------------------------------------')
+    print ('PROCESSING INFO')
+    print ()
+    print ('Generating fractal image of size {0} x {1} with {2} interactions using {3}'.format(width, height, maxIt, device)) 
+       
+    #Define the buffer on the device
+    d_out = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, width * height * np.dtype(np.float64).itemsize)
+
+    #Compile the KernelSource where the fractal will be calculated by the mandelbrot kernel
+    program = cl.Program(context, KernelSource).build()
+    mandelbrot = program.mandelbrot
+    mandelbrot.set_scalar_arg_dtypes([np.int32, np.int32, np.int32, None])
+
+    start = timer()
+
+    #Run the Mandelbrot kernel cointained in KernelSource
+    globalrange = (width, height)
+    localrange = None
+    mandelbrot(queue, globalrange, localrange, width, height, maxIt, d_out)
+    queue.finish()
+
+    #Copy the values stored in the buffer to the host-program.
+    img = np.empty((height, width), dtype=np.int32) # create an array without initializing the entries of given shape and type
+    cl.enqueue_copy(queue, img, d_out) 
+
+    #Display the fractal image
+    fractal = 255.0 * (img / np.max(img))
+    duration = timer() - start
+    print(f'Total processing time: {duration:.2f} seconds')  
+    print ('---------------------------------------------------------------------------------------------------------------------------------------------------------')
+    print ()  
+    plt.axis("off") # disables the display of the number line of the axes
+    plt.imshow(fractal, plt.cm.get_cmap('twilight_shifted')) # plots the mandelbrot figure through the colorset indicated
+    plt.show() # displays the mandelbrot figure
+
+
+if __name__ == "__main__":
+    main()
